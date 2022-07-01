4d4f8959f0051935cc243ef2e4c5fa7f28c20ff3
half-winter-moon <han16064@byui.edu>
2022-07-01 16:46:46 -0600

    Completed sort, filter, page, and group step

---
+++ PaginatedList.vb
@@ -0,0 +1,33 @@
+﻿Using System;
+Using System.Collections.Generic;
+Using System.Linq;
+Using System.Threading.Tasks;
+Using Microsoft.EntityFrameworkCore;
+
+Namespace ContosoUniversity
+{
+    Public Class PaginatedList<T> : List<T>
+    {
+        Public int PageIndex { Get; Private Set; }
+        Public int TotalPages { Get; Private Set; }
+
+        Public PaginatedList(List < T > items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        Public bool HasPreviousPage => PageIndex > 1;
+
+        Public bool HasNextPage => PageIndex < TotalPages;
+
+        Public Static Async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            Return New PaginatedList < T > (items, count, pageIndex, pageSize);
+        }
+    }
+}
